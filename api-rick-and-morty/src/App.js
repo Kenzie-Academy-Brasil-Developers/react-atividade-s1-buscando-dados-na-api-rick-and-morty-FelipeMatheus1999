4363fe4604910b1509aa7395959671582b53f313
Felipe Matheus <felipe_cavalcante7939@hotmail.com>
2021-09-10 18:12:03 -0300

    Melhorando atividade

--- api-rick-and-morty/src/App.js
+++ api-rick-and-morty/src/App.js
@@ -5,19 +5,38 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [characterList, setCharacterList] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
+    fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
       .then((res) => res.json())
       .then((res) => setCharacterList(res.results))
-      .catch((err) => err);
-  }, []);
+      .catch((err) => console.log(err));
+  }, [page]);
+
+  const previousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (page < 34) {
+      setPage(page + 1);
+    }
+  };
 
   return (
     <div className="App">
       <div className="App-header">
         <Characters CharCard={CharCard} />
         <CharCard characterList={characterList} />
+        <button onClick={previousPage} className="button">
+          Last page
+        </button>
+        <button onClick={nextPage} className="button">
+          Next page
+        </button>
       </div>
     </div>
   );
